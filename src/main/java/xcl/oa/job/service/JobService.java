dd27b3bbdc1e3c5009e4aab87d2318dc5277cb14
CHR <CHR@CHR-PC>
2018-05-22 20:46:08 +0800

    添加方法

--- src/main/java/xcl/oa/job/service/JobService.java
+++ src/main/java/xcl/oa/job/service/JobService.java
@@ -1,5 +1,11 @@
 package xcl.oa.job.service;
 
+import java.util.List;
+
+import xcl.oa.job.vo.Job;
+
 public interface JobService {
+
+	List<Job> findAll();
 
 }
--- src/main/java/xcl/oa/job/service/impl/IJobService.java
+++ src/main/java/xcl/oa/job/service/impl/IJobService.java
@@ -1,7 +1,16 @@
 package xcl.oa.job.service.impl;
 
+import java.util.List;
+
 import xcl.oa.job.service.JobService;
+import xcl.oa.job.vo.Job;
 
 public class IJobService implements JobService {
+
+	@Override
+	public List<Job> findAll() {
+		// TODO 自动生成的方法存根
+		return null;
+	}
 
 }
